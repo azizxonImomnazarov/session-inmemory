a6fffbfd9bb0d874b2a9ae103f9c7cf007a6a01a
Azizkhon Imomnazarov <azizxonmamurovich202@gmail.com>
2022-03-15 12:28:33 -0700

    initial commit

---
+++ src/main/java/uz/base/sessioninmemory/SessionInmemoryApplication.java
@@ -0,0 +1,13 @@
+package uz.base.sessioninmemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionInmemoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SessionInmemoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/base/sessioninmemory/SessionInmemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.base.sessioninmemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionInmemoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
